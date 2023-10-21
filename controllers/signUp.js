e53de95b40d6de0852534a518a50bcfa795137e8
n-hassan01 <naimul.hassan.se@gmail.com>
2023-10-21 16:30:57 +0600

    cod clean up

--- controllers/signUp.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", async (req, res, next) => {
-  console.log("Come to the user sign up page ");
-});
-
-module.exports = router;
