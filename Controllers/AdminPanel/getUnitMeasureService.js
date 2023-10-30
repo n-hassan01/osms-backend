8c46ca164ac35de6e53e0ee15ae694efaff808bc
n-hassan01 <naimul.hassan.se@gmail.com>
2023-10-30 13:18:50 +0600

    RS-40 get unit measures service

---
+++ Controllers/AdminPanel/getUnitMeasureService.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const pool = require("../../dbConnection");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  await pool.query(
+    "SELECT * FROM mtl_units_of_measure ORDER BY unit_of_measure ASC ",
+    (error, result) => {
+      try {
+        if (error) throw error;
+
+        res.status(200).json(result.rows);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+});
+
+module.exports = router;
