e7eedcb6ffa88813169a44af7e37667cbb0df8f2
n-hassan01 <naimul.hassan.se@gmail.com>
2023-11-06 16:31:56 +0600

    code clean up

--- Controllers/AdminPanel/addItemMasterService.js
+++ Controllers/AdminPanel/addItemMasterService.js
@@ -4,119 +4,119 @@ const pool = require("../../dbConnection");
 const router = express.Router();
 
 router.post("/", async (req, res, next) => {
-  try {
-    const schema = Joi.object({
-      inventoryItemId: Joi.number().max(999999999999999).required(),
-      organizationId: Joi.number().max(999999999999999).required(),
-      inventoryItemCode: Joi.string().max(40).required(),
-      description: Joi.string().max(240),
-      primaryUomCode: Joi.string().max(3),
-      primaryUnitOfMeasure: Joi.string().max(25),
-      lastUpdateDate: Joi.string().required(),
-      lastUpdatedBy: Joi.string().required(),
-      creationDate: Joi.string().required(),
-      createdBy: Joi.string().required(),
-      lastUpdateLogin: Joi.number(),
-      startDateActive: Joi.string().min(0),
-      endDateActive: Joi.string().min(0),
-      buyerId: Joi.number().max(999999999999999),
-      segment1: Joi.string().max(40),
-      segment2: Joi.string().max(40),
-      segment3: Joi.string().max(40),
-      segment4: Joi.string().max(40),
-      segment5: Joi.string().max(40),
-      purchasingItemFlag: Joi.string().max(1).required(),
-      serviceItemFlag: Joi.string().max(1).required(),
-      inventoryItemFlag: Joi.string().max(1).required(),
-      allowItemDescUpdateFlag: Joi.string().max(1),
-      inspectionRequiredFlag: Joi.string().max(1),
-      receiptRequiredFlag: Joi.string().max(1),
-      rfqRequiredFlag: Joi.string().max(1),
-      qtyRcvTolerance: Joi.number(),
-      unitOfIssue: Joi.string().max(25),
-      daysEarlyReceiptAllowed: Joi.number(),
-      daysLateReceiptAllowed: Joi.number(),
-      receivingRoutingId: Joi.number(),
-      shelfLifeCode: Joi.number(),
-      shelfLifeDays: Joi.number(),
-      sourceOrganizationId: Joi.number(),
-      sourceSubInventory: Joi.string().max(10),
-      acceptableEarlyDays: Joi.number(),
-      fixedLeadTime: Joi.number(),
-      variableLeadTime: Joi.number(),
-      minMinmaxQuantity: Joi.number(),
-      maxMinmaxQuantity: Joi.number(),
-      minimumOrderQuantity: Joi.number(),
-      maximumOrderQuantity: Joi.number(),
-      paymentTermsId: Joi.number(),
-    });
-    const validation = schema.validate(req.body);
+  const schema = Joi.object({
+    inventoryItemId: Joi.number().max(999999999999999).required(),
+    // organizationId: Joi.number().max(999999999999999).required(),
+    inventoryItemCode: Joi.string().max(40).required(),
+    description: Joi.string().max(240).min(0),
+    primaryUomCode: Joi.string().max(3).min(0),
+    primaryUnitOfMeasure: Joi.string().max(25).min(0),
+    lastUpdateDate: Joi.string().required(),
+    lastUpdatedBy: Joi.string().required(),
+    creationDate: Joi.string().required(),
+    createdBy: Joi.string().required(),
+    lastUpdateLogin: Joi.number().allow(null),
+    enabledFlag: Joi.string().max(1).required(),
+    startDateActive: Joi.string().min(0),
+    endDateActive: Joi.string().min(0),
+    buyerId: Joi.number().max(999999999).allow(null),
+    segment1: Joi.string().max(40).min(0),
+    segment2: Joi.string().max(40).min(0),
+    segment3: Joi.string().max(40).min(0),
+    segment4: Joi.string().max(40).min(0),
+    segment5: Joi.string().max(40).min(0),
+    purchasingItemFlag: Joi.string().max(1).required(),
+    serviceItemFlag: Joi.string().max(1).required(),
+    inventoryItemFlag: Joi.string().max(1).required(),
+    allowItemDescUpdateFlag: Joi.string().max(1).min(0),
+    inspectionRequiredFlag: Joi.string().max(1).min(0),
+    receiptRequiredFlag: Joi.string().max(1).min(0),
+    rfqRequiredFlag: Joi.string().max(1).min(0),
+    qtyRcvTolerance: Joi.number().allow(null),
+    unitOfIssue: Joi.string().max(25).min(0),
+    daysEarlyReceiptAllowed: Joi.number().allow(null),
+    daysLateReceiptAllowed: Joi.number().allow(null),
+    receivingRoutingId: Joi.number().allow(null),
+    shelfLifeCode: Joi.number().allow(null),
+    shelfLifeDays: Joi.number().allow(null),
+    sourceOrganizationId: Joi.number().allow(null),
+    sourceSubInventory: Joi.string().max(10),
+    acceptableEarlyDays: Joi.number().allow(null),
+    fixedLeadTime: Joi.number().allow(null),
+    variableLeadTime: Joi.number().allow(null),
+    minMinmaxQuantity: Joi.number().allow(null),
+    maxMinmaxQuantity: Joi.number().allow(null),
+    minimumOrderQuantity: Joi.number().allow(null),
+    maximumOrderQuantity: Joi.number().allow(null),
+    paymentTermsId: Joi.number().allow(null),
+  });
+  const validation = schema.validate(req.body);
 
-    if (validation.error) {
-      console.log(validation.error);
+  if (validation.error) {
+    console.log(validation.error);
 
-      res.status(400).send("Invalid inputs");
-    }
+    res.status(400).send("Invalid inputs");
+  }
 
-    const {
-      inventoryItemId,
-      organizationId,
-      inventoryItemCode,
-      description,
-      primaryUomCode,
-      primaryUnitOfMeasure,
-      lastUpdateDate,
-      lastUpdatedBy,
-      creationDate,
-      createdBy,
-      lastUpdateLogin,
-      startDateActive,
-      endDateActive,
-      buyerId,
-      segment1,
-      segment2,
-      segment3,
-      segment4,
-      segment5,
-      purchasingItemFlag,
-      serviceItemFlag,
-      inventoryItemFlag,
-      allowItemDescUpdateFlag,
-      inspectionRequiredFlag,
-      receiptRequiredFlag,
-      rfqRequiredFlag,
-      qtyRcvTolerance,
-      unitOfIssue,
-      daysEarlyReceiptAllowed,
-      daysLateReceiptAllowed,
-      receivingRoutingId,
-      shelfLifeCode,
-      shelfLifeDays,
-      sourceOrganizationId,
-      sourceSubInventory,
-      acceptableEarlyDays,
-      fixedLeadTime,
-      variableLeadTime,
-      minMinmaxQuantity,
-      maxMinmaxQuantity,
-      minimumOrderQuantity,
-      maximumOrderQuantity,
-      paymentTermsId,
-    } = req.body;
+  const {
+    inventoryItemId,
+    inventoryItemCode,
+    description,
+    primaryUomCode,
+    primaryUnitOfMeasure,
+    lastUpdateDate,
+    lastUpdatedBy,
+    creationDate,
+    createdBy,
+    lastUpdateLogin,
+    enabledFlag,
+    startDateActive,
+    endDateActive,
+    buyerId,
+    segment1,
+    segment2,
+    segment3,
+    segment4,
+    segment5,
+    purchasingItemFlag,
+    serviceItemFlag,
+    inventoryItemFlag,
+    allowItemDescUpdateFlag,
+    inspectionRequiredFlag,
+    receiptRequiredFlag,
+    rfqRequiredFlag,
+    qtyRcvTolerance,
+    unitOfIssue,
+    daysEarlyReceiptAllowed,
+    daysLateReceiptAllowed,
+    receivingRoutingId,
+    shelfLifeCode,
+    shelfLifeDays,
+    sourceOrganizationId,
+    sourceSubInventory,
+    acceptableEarlyDays,
+    fixedLeadTime,
+    variableLeadTime,
+    minMinmaxQuantity,
+    maxMinmaxQuantity,
+    minimumOrderQuantity,
+    maximumOrderQuantity,
+    paymentTermsId,
+  } = req.body;
 
-    await pool.query(
-      "SELECT COUNT(*) FROM mtl_system_items WHERE inventory_item_id = $1",
-      [inventoryItemId],
-      (error, result) => {
-        if (error) throw error;
+  pool.query(
+    "SELECT COUNT(*) FROM mtl_system_items WHERE inventory_item_id = $1",
+    [inventoryItemId],
+    (error, result) => {
+      const countExistUserId = result.rows[0].count;
 
-        const countExistUserId = result.rows[0].count;
-        if (countExistUserId === "0") {
+      if (countExistUserId === "0") {
+        for (let index = 1; index < 6; index++) {
           pool.query(
             "INSERT INTO mtl_system_items(inventory_item_id, organization_id, inventory_item_code, description, primary_uom_code, primary_unit_of_measure, last_update_date, last_updated_by, creation_date, created_by, last_update_login, enabled_flag, start_date_active, end_date_active, buyer_id, segment1, segment2, segment3, segment4, segment5, purchasing_item_flag, service_item_flag, inventory_item_flag, allow_item_desc_update_flag, inspection_required_flag, receipt_required_flag, rfq_required_flag, qty_rcv_tolerance, unit_of_issue, days_early_receipt_allowed, days_late_receipt_allowed, receiving_routing_id, shelf_life_code, shelf_life_days, source_organization_id, source_subinventory, acceptable_early_days, fixed_lead_time, variable_lead_time, min_minmax_quantity, max_minmax_quantity, minimum_order_quantity, maximum_order_quantity, payment_terms_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44);",
             [
               inventoryItemId,
-              organizationId,
+              index,
               inventoryItemCode,
               description,
               primaryUomCode,
@@ -126,7 +126,7 @@ router.post("/", async (req, res, next) => {
               creationDate,
               createdBy,
               lastUpdateLogin,
-              "y",
+              enabledFlag,
               startDateActive,
               endDateActive,
               buyerId,
@@ -161,19 +161,21 @@ router.post("/", async (req, res, next) => {
               paymentTermsId,
             ],
             (error, result) => {
-              if (error) throw error;
-
-              res.status(200).json({ message: "Successfully added!" });
+              try {
+                if (error) throw error;
+              } catch (err) {
+                console.log(err.message);
+                next(err);
+              }
             }
           );
-        } else {
-          res.status(400).json({ message: "Bad request!" });
         }
+        res.status(200).json({ message: "Successfully added!" });
+      } else {
+        res.status(400).json({ message: "Bad request!" });
       }
-    );
-  } catch (err) {
-    next(err);
-  }
+    }
+  );
 });
 
 module.exports = router;
