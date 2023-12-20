742bf4f27f8539c07302a789b771d9c588ddafd4
n-hassan01 <naimul.hassan.se@gmail.com>
2023-12-20 17:21:13 +0600

    code correction

--- Controllers/getProfileDetailsService.js
+++ Controllers/getProfileDetailsService.js
@@ -7,7 +7,8 @@ router.get("/", async (req, res, next) => {
   const id = req.id;
 
   await pool.query(
-    "select * from per_all_peoples p INNER JOIN fnd_user f on p.person_id = f.employee_id where  f.user_name = $1",
+    // "select * from per_all_peoples p INNER JOIN fnd_user f on p.person_id = f.employee_id where  f.user_name = $1",
+    "select * from per_all_peoples p full JOIN fnd_user f on p.employee_number = f.user_name full JOIN hz_cust_accounts c ON c.account_number = f.user_name where f.user_name = $1",
     [id],
     (error, result) => {
       try {
