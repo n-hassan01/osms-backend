8e47c618ebd5bd3670752af051a141055543aa0d
Remark2023 <app.remarkhb@gmail.com>
2024-01-18 10:51:57 +0600

    code clean up

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ app.use("/sales-order-line", SalesOrderLineService);
 app.use("/so-approval", SoApprovalService);
 app.use("/wf-notification-view", WfNotificationView);
 app.use("/create-salesorder-number", CreateSalesOrderNumberSevice);
-app.use("/customer-list", GetCustomerListService);
+app.use("/customer-list", AuthGuard, GetCustomerListService);
 
 // routing middleware for admin
 app.use("/add-hr-locations-all", AddHrLocationsAll);
