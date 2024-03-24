3845e9947311fcac45c9701afd4b8c3a233bf7b9
Ahmed Raihan <ahmedraihanalif@gmail.com>
2024-03-24 15:28:33 +0600

    addDrillDown Service

---
+++ Controllers/AdminPanel/getDrillDown.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const Joi = require("joi");
+const pool = require("../../dbConnection");
+const router = express.Router();
+
+
+router.get("/view", async (req, res, next) => {
+  await pool.query("SELECT  sum(amount), deposit_type_name, company_name,company_account,  customer_name FROM public.customer_deposit_all_v Group by deposit_type_name,   company_bank, company_account, company_name, employee_name, customer_deposit_all_v.customer_name", (error, result) => {
+    try {
+      if (error) throw error;
+      res.status(200).send(result.rows);
+    } catch (err) {
+      next(err);
+    }
+  });
+});
+
+
+module.exports = router;
