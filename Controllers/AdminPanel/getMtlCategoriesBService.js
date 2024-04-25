5200204bbef146b89d46ee4e86e637b5640fb29f
Ahmed Raihan <ahmedraihanalif@gmail.com>
2024-04-25 11:20:31 +0600

    addMultiDynamicDivisionDistrictThana

---
+++ Controllers/AdminPanel/getMtlCategoriesBService.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const pool = require("../../dbConnection");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  await pool.query(
+    "SELECT category_id, description FROM mtl_categories_b",
+
+    (error, result) => {
+      try {
+        if (error) throw error;
+
+        res.status(200).json(result.rows);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+});
+
+module.exports = router;
