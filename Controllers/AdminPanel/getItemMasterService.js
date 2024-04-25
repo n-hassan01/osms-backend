f1c4156c72dd96ddb944fc78b4cabd55479a352e
Remark2023 <app.remarkhb@gmail.com>
2024-04-25 16:31:14 +0600

    dev

--- Controllers/AdminPanel/getItemMasterService.js
+++ Controllers/AdminPanel/getItemMasterService.js
@@ -36,6 +36,23 @@ router.get("/inventory_item_id", async (req, res, next) => {
   );
 });
 
+router.get("/:category_id", async (req, res, next) => {
+  const categoryId = req.params.category_id;
+  await pool.query(
+    "SELECT inventory_item_id, description FROM mtl_system_items where category_id=$1",
+    [categoryId],
+    (error, result) => {
+      try {
+        if (error) throw error;
+
+        res.status(200).json(result.rows);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+});
+
 router.get("/list", async (req, res, next) => {
   await pool.query(
     "SELECT inventory_item_id, description FROM mtl_system_items;",
