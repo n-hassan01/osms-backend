bf07002ba39ca1e64fab8df7a9c48d5f4ab3501b
Ahmed Raihan <ahmedraihanalif@gmail.com>
2024-09-12 12:37:07 +0600

    add summary api

---
+++ Controllers/AdminPanel/getSummaryCustomerListService.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const pool = require("../../dbConnection");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  await pool.query(
+    "SELECT customer_group, sum(amount) amount FROM public.customer_deposit_all_v GROUP BY customer_group ORDER BY customer_group;",
+    (error, result) => {
+      try {
+        if (error) throw error;
+
+        res.status(200).json(result.rows);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+});
+module.exports = router;
