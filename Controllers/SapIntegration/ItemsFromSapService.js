89e2d4eccc2dcf95eade76e4494a2eade71be64e
n-hassan01 <naimul.hassan.se@gmail.com>
2024-09-26 10:03:21 +0600

    dev

---
+++ Controllers/SapIntegration/ItemsFromSapService.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const pool = require("../../dbConnection");
+const router = express.Router();
+const axios = require("axios");
+const Joi = require("joi");
+
+const username = process.env.USERNAME_TO_GET_CUSTOMER_FROM_SAP;
+const password = process.env.PASSWORD_TO_GET_CUSTOMER_FROM_SAP;
+
+router.get("/", async (req, res, next) => {
+  const auth = Buffer.from(`${username}:${password}`).toString("base64");
+
+  try {
+    const response = await axios.get(
+      "https://my411141-api.s4hana.cloud.sap/sap/opu/odata4/sap/api_product/srvd_a2x/sap/product/0002/Product",
+      {
+        headers: {
+          Authorization: `Basic ${auth}`,
+        },
+      }
+    );
+
+    const items = response.data.value;
+    res.json({ items });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post("/add", async (req, res, next) => {
+  const schema = Joi.object({
+    businessPartner: Joi.string().min(0),
+    businessPartnerFullname: Joi.string().min(0),
+    businessPartnerCategory: Joi.number().allow(null),
+    businessPartnerGrouping: Joi.string().min(0),
+    businessPartnerIdByExtSystem: Joi.string().min(0),
+    businessPartnerType: Joi.string().min(0),
+  });
+
+  const validation = schema.validate(req.body);
+
+  if (validation.error) {
+    console.log(validation.error);
+
+    return res.status(400).send("Invalid inputs");
+  }
+
+  const {
+    businessPartner,
+    businessPartnerFullname,
+    businessPartnerCategory,
+    businessPartnerGrouping,
+    businessPartnerIdByExtSystem,
+    businessPartnerType,
+  } = req.body;
+
+  const date = new Date();
+
+  await pool.query(
+    "INSERT INTO public.customer_from_sap(business_partner, business_partner_category, business_partner_fullname, business_partner_grouping, business_partner_id_by_ext_system, business_partner_type) VALUES ($1, $2, $3, $4, $5, $6);",
+    [
+      businessPartner,
+      businessPartnerCategory,
+      businessPartnerFullname,
+      businessPartnerGrouping,
+      businessPartnerIdByExtSystem,
+      businessPartnerType,
+    ],
+    (error, result) => {
+      try {
+        if (error) throw error;
+
+        return res.status(200).json({ message: "Successfully added!" });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+});
+
+router.post("/add/error", async (req, res, next) => {
+  const schema = Joi.object({
+    businessPartner: Joi.string().min(0),
+    errorCode: Joi.string().min(0),
+    errorMessage: Joi.string().min(0),
+  });
+
+  const validation = schema.validate(req.body);
+
+  if (validation.error) {
+    console.log(validation.error);
+
+    return res.status(400).send("Invalid inputs");
+  }
+
+  const { businessPartner, errorCode, errorMessage } = req.body;
+
+  const date = new Date();
+
+  await pool.query(
+    "INSERT INTO public.error_captured(business_partner, error_code, error_message) VALUES ($1, $2, $3);",
+    [businessPartner, errorCode, errorMessage],
+    (error, result) => {
+      try {
+        if (error) throw error;
+
+        return res.status(200).json({ message: "Successfully added!" });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+});
+
+module.exports = router;
