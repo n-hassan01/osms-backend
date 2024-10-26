4f4a3f3dcfb8925aa39ca961f2ae939265c48c13
Ahmed Raihan <ahmedraihanalif@gmail.com>
2024-10-26 14:14:44 +0600

    add new api

--- Controllers/AdminPanel/getTerritoryInsights.js
+++ Controllers/AdminPanel/getTerritoryInsights.js
@@ -2,9 +2,9 @@ const express = require("express");
 const pool = require("../../dbConnection");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/allIds", async (req, res, next) => {
   await pool.query(
-    "select * from territory_insights;",
+    "SELECT t.territory_id,t.territory_name,t.tsm_code,p.full_name FROM public.territory t , per_all_peoples p where t.tsm_code=p.employee_number;",
 
     (error, result) => {
       try {
@@ -18,82 +18,73 @@ router.get("/", async (req, res, next) => {
   );
 });
 
-router.post("/percompetitors", async (req, res, next) => {
-  const { territory_id } = req.body;
+router.post("/territoryLists", async (req, res, next) => {
+  // const { territory_id } = req.body;
 
   await pool.query(
-    `WITH CompetitorData AS (
-      SELECT 
-        t.territory_id,
-        tc.competitor_name,
-        tc.competitor_monthly_sales,
-        tc.bill_board_count
-      FROM public.territory t 
-      JOIN public.territory_competitors tc ON t.territory_id = tc.territory_id
-    )
-    SELECT 
-      t.region_id, 
-      t.area_id, 
-      t.territory_id, 
-      t.territory_code, 
-      t.tsm_code, 
-      t.territory_name, 
-      tn.town_id, 
-      tn.cust_account_id, 
-      tn.town_code, 
-      tn.town_name, 
-      tn.ambassador_code, 
-      tn.ambassador_name, 
-      ti.creation_date AS territory_creation_date, 
-      ti.created_by AS territory_created_by, 
-      ti.last_update_date AS territory_last_update_date, 
-      ti.last_updated_by AS territory_last_updated_by, 
-      ti.last_update_login AS territory_last_update_login, 
-      ti.distributor_count, 
-      ti.sales_officer_count, 
-      ti.total_outlet_count, 
-      ti.company_outlet_count, 
-      ti.population_count, 
-      ti.monthly_sales_actual, 
-      ti.monthly_sales_target, 
-      ti.monthly_collection_actual, 
-      ti.monthly_collection_target, 
-      ARRAY_TO_STRING(ARRAY_AGG(cd.competitor_name), ', ') AS competitor_names,  -- Comma-separated competitor names
-      ARRAY_TO_STRING(ARRAY_AGG(cd.competitor_monthly_sales), ', ') AS competitor_monthly_sales,  -- Comma-separated competitor monthly sales
-      ARRAY_TO_STRING(ARRAY_AGG(cd.bill_board_count), ', ') AS competitor_bill_board_counts  -- Comma-separated billboard counts
-    FROM public.territory t 
-    JOIN public.town tn ON t.territory_id = tn.territory_id 
-    JOIN public.territory_insights ti ON t.territory_id = ti.territory_id 
-    LEFT JOIN CompetitorData cd ON t.territory_id = cd.territory_id  
-    WHERE t.territory_id = $1
-    GROUP BY 
-      t.region_id, 
-      t.area_id, 
-      t.territory_id, 
-      t.territory_code, 
-      t.tsm_code, 
-      t.territory_name, 
-      tn.town_id, 
-      tn.cust_account_id, 
-      tn.town_code, 
-      tn.town_name, 
-      tn.ambassador_code, 
-      tn.ambassador_name, 
-      ti.creation_date, 
-      ti.created_by, 
-      ti.last_update_date, 
-      ti.last_updated_by, 
-      ti.last_update_login, 
-      ti.distributor_count, 
-      ti.sales_officer_count, 
-      ti.total_outlet_count, 
-      ti.company_outlet_count, 
-      ti.population_count, 
-      ti.monthly_sales_actual, 
-      ti.monthly_sales_target, 
-      ti.monthly_collection_actual, 
-      ti.monthly_collection_target;`,
-    [territory_id],
+    `SELECT 
+    t.territory_id, 
+    t.territory_name, 
+    tn.town_name, 
+    tn.ambassador_name, 
+    p.full_name
+FROM 
+    territory t 
+JOIN 
+    town tn ON t.territory_id = tn.territory_id 
+JOIN 
+    per_all_peoples p ON t.tsm_code = p.employee_number
+WHERE 
+    t.territory_id = 1;
+`,
+    //  [territory_id],
+
+    (error, result) => {
+      try {
+        if (error) throw error;
+
+        res.status(200).json(result.rows);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+});
+
+router.post("/perInsights", async (req, res, next) => {
+  // const { territory_id } = req.body;
+
+  await pool.query(
+    `SELECT t.territory_id, t.territory_name, pap.full_name tsm_name,
+ti.distributor_count, ti.sales_officer_count, 
+    ti.total_outlet_count, ti.company_outlet_count, 
+    ti.population_count, ti.monthly_sales_actual, ti.monthly_sales_target, 
+	ti.monthly_collection_actual, ti.monthly_collection_target
+    FROM territory t 
+    JOIN territory_insights ti ON t.territory_id = ti.territory_id 
+    JOIN per_all_peoples pap ON t.tsm_code = pap.employee_number;`,
+    //  [territory_id],
+
+    (error, result) => {
+      try {
+        if (error) throw error;
+
+        res.status(200).json(result.rows);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+});
+
+router.post("/perCompetitors", async (req, res, next) => {
+  // const { territory_id } = req.body;
+
+  await pool.query(
+    `SELECT t.territory_id, tc.competitor_name, tc.competitor_monthly_sales, tc.bill_board_count 
+FROM public.territory t 
+JOIN public.territory_competitors tc ON t.territory_id = tc.territory_id;`,
+    //  [territory_id],
 
     (error, result) => {
       try {
