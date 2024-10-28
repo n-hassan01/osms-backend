350ba3c6e0ca3e54a1eb89815e95dca023a1f149
n-hassan01 <naimul.hassan.se@gmail.com>
2024-10-28 17:09:16 +0600

    dev

--- Controllers/BandingAssetsManagement/territoryService.js
+++ Controllers/BandingAssetsManagement/territoryService.js
@@ -32,4 +32,13 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/view", async (req, res, next) => {
+  try {
+    const result = await pool.query("SELECT * FROM territory_insights_lines_v;");
+    res.status(200).json(result.rows);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
